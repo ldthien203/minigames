98e46f134a253ad1f7610ba0ed8b227c1b38be06
ldthien203 <96572606+ldthien203@users.noreply.github.com>
2025-05-07 15:41:40 +0700

    add pagination to ReviewSection

--- client/src/pages/Reviews/components/ReviewSection/ReviewSection.jsx
+++ client/src/pages/Reviews/components/ReviewSection/ReviewSection.jsx
@@ -1,8 +1,9 @@
 import {Fragment, useMemo} from 'react'
 import {useSearchParams} from 'react-router-dom'
-import IntroCard from '../../../../components/IntroCard/IntroCard'
-import './ReviewSection.css'
 import useFetchData from '../../../../hooks/useFetchData'
+import IntroCard from '../../../../components/IntroCard/IntroCard'
+import PaginationWrapper from '../../../../components/PaginationWrapper/PaginationWrapper'
+import './ReviewSection.css'
 
 const ReviewSection = () => {
   const [searchParams] = useSearchParams()
@@ -26,27 +27,32 @@ const ReviewSection = () => {
         <div className="container">
           {loading && <p>Loading reviews...</p>}
           {error && <p>{error}</p>}
-          {reviews &&
-            reviews.map(card => (
-              <div className="review-item" key={card.game_id}>
-                <IntroCard
-                  key={card.game_id}
-                  date={card.release_date}
-                  id={card.game_id}
-                  title={card.name}
-                  category={card.category_name}
-                  description={card.summary}
-                  isShowImg={true}
-                  isShowRating={true}
-                  img={card.thumbnail}
-                  rating={card.avg_rating}
-                  categoryLink={`/${card.category_name.toLowerCase()}`}
-                  readMoreLink={`/${card.category_name.toLowerCase()}/${
-                    card.game_id
-                  }`}
-                />
-              </div>
-            ))}
+          {reviews && (
+            <PaginationWrapper data={reviews} pageSize={8}>
+              {currentTable =>
+                currentTable.map(card => (
+                  <div className="review-item" key={card.game_id}>
+                    <IntroCard
+                      key={card.game_id}
+                      date={card.release_date}
+                      id={card.game_id}
+                      title={card.name}
+                      category={card.category_name}
+                      description={card.summary}
+                      isShowImg={true}
+                      isShowRating={true}
+                      img={card.thumbnail}
+                      rating={card.avg_rating}
+                      categoryLink={`/${card.category_name.toLowerCase()}`}
+                      readMoreLink={`/${card.category_name.toLowerCase()}/${
+                        card.game_id
+                      }`}
+                    />
+                  </div>
+                ))
+              }
+            </PaginationWrapper>
+          )}
         </div>
       </section>
     </Fragment>
